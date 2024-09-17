27e0b93b7fe5921e8a9e939c036721dc5917bdf1
apirrone <awanegenebayo@gmail.com>
2024-09-17 15:32:48 +0200

    only forward reference

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -35,8 +35,8 @@ from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobot
 # MOTION_FILES = glob.glob("datasets/bdx/new_placo_moves/*")
 MOTION_FILES = [
     "datasets/bdx/new_new_placo_moves/bdx_walk_forward.txt",
-    "datasets/bdx/new_new_placo_moves/bdx_turn_left.txt",
-    "datasets/bdx/new_new_placo_moves/bdx_turn_right.txt",
+    # "datasets/bdx/new_new_placo_moves/bdx_turn_left.txt",
+    # "datasets/bdx/new_new_placo_moves/bdx_turn_right.txt",
     # "datasets/bdx/new_placo_moves/bdx_walk_forward.txt", # OK
 ]
 
