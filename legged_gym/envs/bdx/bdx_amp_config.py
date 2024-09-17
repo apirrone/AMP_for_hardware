9a2f021e8053b715969ee166690b4187d89c22a2
apirrone <awanegenebayo@gmail.com>
2024-09-17 17:57:27 +0200

    base height reward

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -245,7 +245,7 @@ class BDXAMPCfg(LeggedRobotCfg):
             torques = -0.000025  # -0.000025
             dof_vel = 0.0
             dof_acc = 0.0
-            base_height = 0.0
+            base_height = -1.0
             feet_air_time = 0.0
             collision = 0.0
             feet_stumble = 0.0
