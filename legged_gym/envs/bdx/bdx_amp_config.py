a654d3e11465c2d55a173ab2f031ce36ae8bc5c2
apirrone <awanegenebayo@gmail.com>
2024-09-17 16:10:00 +0200

    adding noise and randomization

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -201,31 +201,31 @@ class BDXAMPCfg(LeggedRobotCfg):
         substeps = 1
 
     class domain_rand:
-        randomize_friction = False
-        friction_range = [0.9, 1.1]
-        randomize_base_mass = False
-        added_mass_range = [-0.1, 0.1]
+        randomize_friction = True
+        friction_range = [0.95, 1.05]
+        randomize_base_mass = True
+        added_mass_range = [-0.05, 0.05]
         push_robots = False
         push_interval_s = 15
         max_push_vel_xy = 0.5  # 0.3
         randomize_gains = False
         stiffness_multiplier_range = [0.95, 1.05]
         damping_multiplier_range = [0.95, 1.05]
-        randomize_torques = False
-        torque_multiplier_range = [0.9, 1.1]
-        randomize_com = False
-        com_range = [-0.02, 0.02]
+        randomize_torques = True
+        torque_multiplier_range = [0.95, 1.05]
+        randomize_com = True
+        com_range = [-0.01, 0.01]
 
     class noise:
-        add_noise = False
+        add_noise = True
         noise_level = 1.0  # scales other values
 
         class noise_scales:
-            dof_pos = 0.05
-            dof_vel = 0.05  # 1.5
-            lin_vel = 0.05
-            ang_vel = 0.05
-            gravity = 0.05
+            dof_pos = 0.01
+            dof_vel = 0.01  # 1.5
+            lin_vel = 0.01
+            ang_vel = 0.01
+            gravity = 0.01
             height_measurements = 0.1
 
     class rewards(LeggedRobotCfg.rewards):
--- legged_gym/scripts/play.py
+++ legged_gym/scripts/play.py
@@ -45,18 +45,18 @@ def play(args):
     env_cfg.env.num_envs = min(env_cfg.env.num_envs, 50)
 
     env_cfg.env.get_commands_from_joystick = False
-    env_cfg.env.get_commands_from_keyboard = True
+    env_cfg.env.get_commands_from_keyboard = False
 
     env_cfg.terrain.num_rows = 5
     env_cfg.terrain.num_cols = 5
     env_cfg.terrain.curriculum = False
-    env_cfg.noise.add_noise = True
-    env_cfg.domain_rand.randomize_friction = True
+    env_cfg.noise.add_noise = False
+    env_cfg.domain_rand.randomize_friction = False
     env_cfg.domain_rand.push_robots = False
     env_cfg.domain_rand.randomize_gains = False
     env_cfg.domain_rand.randomize_base_mass = False  # TODO
-    env_cfg.domain_rand.randomize_torques = True  # TODO
-    env_cfg.domain_rand.randomize_com = True  # TODO
+    env_cfg.domain_rand.randomize_torques = False  # TODO
+    env_cfg.domain_rand.randomize_com = False  # TODO
     # env_cfg.commands.ranges.lin_vel_x = [0.0, 0.1]
     # env_cfg.commands.ranges.ang_vel_yaw = [-0.7, 0.7]
 
