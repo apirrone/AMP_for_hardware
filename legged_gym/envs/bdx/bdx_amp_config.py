22e995336da380bd976bdf8a73a831beedd6169a
apirrone <awanegenebayo@gmail.com>
2024-09-19 22:56:02 +0200

    adding noise and rand

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -224,11 +224,11 @@ class BDXAMPCfg(LeggedRobotCfg):
         noise_level = 1.0  # scales other values
 
         class noise_scales:
-            dof_pos = 0.01
-            dof_vel = 0.01  # 1.5
-            lin_vel = 0.01
-            ang_vel = 0.01
-            gravity = 0.01
+            dof_pos = 0.05
+            dof_vel = 0.05  # 1.5
+            lin_vel = 0.05
+            ang_vel = 0.05
+            gravity = 0.05
             height_measurements = 0.1
 
     class rewards(LeggedRobotCfg.rewards):
