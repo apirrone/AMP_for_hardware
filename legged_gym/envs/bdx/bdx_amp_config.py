06a60ceb3431dff7532e28b3e6a05b2f446e4c62
apirrone <awanegenebayo@gmail.com>
2024-09-20 09:29:06 +0200

    adding noise and rand

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -205,7 +205,7 @@ class BDXAMPCfg(LeggedRobotCfg):
 
     class domain_rand:
         randomize_friction = True
-        friction_range = [0.8, 1.2]
+        friction_range = [0.7, 1.3]
         randomize_base_mass = True
         added_mass_range = [-0.1, 0.1]
         push_robots = False
@@ -215,20 +215,20 @@ class BDXAMPCfg(LeggedRobotCfg):
         stiffness_multiplier_range = [0.95, 1.05]
         damping_multiplier_range = [0.95, 1.05]
         randomize_torques = True
-        torque_multiplier_range = [0.8, 1.2]
+        torque_multiplier_range = [0.7, 1.3]
         randomize_com = True
-        com_range = [-0.05, 0.05]
+        com_range = [-0.1, 0.1]
 
     class noise:
         add_noise = True
         noise_level = 1.0  # scales other values
 
         class noise_scales:
-            dof_pos = 0.05
-            dof_vel = 0.05  # 1.5
-            lin_vel = 0.05
-            ang_vel = 0.05
-            gravity = 0.05
+            dof_pos = 0.08
+            dof_vel = 0.08  # 1.5
+            lin_vel = 0.08
+            ang_vel = 0.08
+            gravity = 0.08
             height_measurements = 0.1
 
     class rewards(LeggedRobotCfg.rewards):
@@ -265,7 +265,7 @@ class BDXAMPCfg(LeggedRobotCfg):
         heading_command = False  # if true: compute ang vel command from heading error
 
         class ranges:
-            lin_vel_x = [0.13, 0.13]  # min max [m/s]
+            lin_vel_x = [0.0, 0.16]  # min max [m/s]
             lin_vel_y = [0.0, 0.0]  # min max [m/s]
             ang_vel_yaw = [0.0, 0.0]  # min max [rad/s]
             heading = [0, 0]
