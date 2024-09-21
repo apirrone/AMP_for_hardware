124d6fb490de9047f7e4a0f5a7e272f3f4b192ea
apirrone <awanegenebayo@gmail.com>
2024-09-21 18:02:52 +0200

    more rand

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -210,9 +210,9 @@ class BDXAMPCfg(LeggedRobotCfg):
 
     class domain_rand:
         randomize_friction = True
-        friction_range = [0.5, 2.0]
+        friction_range = [0.5, 3.0]
         randomize_base_mass = True
-        added_mass_range = [-0.3, 0.3]
+        added_mass_range = [-0.4, 0.4]
         push_robots = False
         push_interval_s = 15
         max_push_vel_xy = 0.5  # 0.3
@@ -220,9 +220,9 @@ class BDXAMPCfg(LeggedRobotCfg):
         stiffness_multiplier_range = [0.95, 1.05]
         damping_multiplier_range = [0.95, 1.05]
         randomize_torques = True
-        torque_multiplier_range = [0.5, 1.5]
+        torque_multiplier_range = [0.3, 1.5]
         randomize_com = True
-        com_range = [-0.15, 0.15]
+        com_range = [-0.2, 0.2]
 
     class noise:
         add_noise = True
