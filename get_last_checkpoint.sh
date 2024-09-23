91a7b749c8c9a3724deeda55bf031dacb0192ebc
apirrone <awanegenebayo@gmail.com>
2024-09-23 11:17:36 +0200

    no amp, turn

--- get_last_checkpoint.sh
+++ get_last_checkpoint.sh
@@ -1,12 +1,18 @@
+if [ $2 = "local" ]; then
+    last=$(ssh apirrone@192.168.1.221 "cd /home/apirrone/MISC/AMP_for_hardware/logs/bdx_amp/*_$1/ ; ls *.pt -lt | head -n 1" | awk '{print $8}')
+else
+    last=$(ssh -p4242 apirrone@s-nguyen.net "cd /home/apirrone/MISC/AMP_for_hardware/logs/bdx_amp/*_$1/ ; ls *.pt -lt | head -n 1" | awk '{print $8}')
+fi
 
-last=$(ssh -p4242 apirrone@s-nguyen.net "cd /home/apirrone/MISC/AMP_for_hardware/logs/bdx_amp/*_$1/ ; ls *.pt -lt | head -n 1" | awk '{print $8}')
 last_local=$(cd logs/bdx_amp/aze/ && ls *.pt -lt | head -n 1 | awk '{print $9}' && cd ../../..)
-
-# if last == last_local, do nothing
 
 if [ "$last" == "$last_local" ]; then
     echo "No new file to download"
     exit 0
 fi
 
-scp -p4242 apirrone@s-nguyen.net:/home/apirrone/MISC/AMP_for_hardware/logs/bdx_amp/*_$1/$last logs/bdx_amp/aze/
+if [ $2 = "local" ]; then
+    scp apirrone@192.168.1.221:/home/apirrone/MISC/AMP_for_hardware/logs/bdx_amp/*_$1/$last logs/bdx_amp/aze/
+else
+    scp -p4242 apirrone@s-nguyen.net:/home/apirrone/MISC/AMP_for_hardware/logs/bdx_amp/*_$1/$last logs/bdx_amp/aze/
+fi
--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -271,8 +271,8 @@ class BDXAMPCfg(LeggedRobotCfg):
 
         class ranges:
             lin_vel_x = [0.14, 0.14]  # min max [m/s]
-            lin_vel_y = [0.0, 0.0]  # min max [m/s]
-            ang_vel_yaw = [0.0, 0.0]  # min max [rad/s]
+            lin_vel_y = [-0.1, 0.1]  # min max [m/s]
+            ang_vel_yaw = [-0.4, 0.4]  # min max [rad/s]
             heading = [0, 0]
             # lin_vel_x = [0.1, 0.2]  # min max [m/s]
             # lin_vel_y = [0.0, 0.0]  # min max [m/s]
@@ -313,7 +313,7 @@ class BDXAMPCfgPPO(LeggedRobotCfgPPO):
         amp_reward_coef = 2.0  # 2.0
         amp_motion_files = MOTION_FILES
         amp_num_preload_transitions = 2000000
-        amp_task_reward_lerp = 0.2  # 0.3
+        amp_task_reward_lerp = 1.0  # 0.3
         amp_discr_hidden_dims = [1024, 512]
 
         disc_grad_penalty = 0.01  # original 10 # TUNE ?
