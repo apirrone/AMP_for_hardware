912bb75add9432bb6bedde904ad6af8ec875a3e2
apirrone <awanegenebayo@gmail.com>
2024-10-01 10:15:57 +0200

    debug lag buffer

--- legged_gym/envs/base/observation_buffer.py
+++ legged_gym/envs/base/observation_buffer.py
@@ -50,14 +50,9 @@ class ObservationBuffer:
         obs = []
         for i, env_id in enumerate(self.obs_buf):
             idx = indices[i]
-            obs.append(self.obs_buf[i, idx * self.num_obs : (idx + 1) * self.num_obs])
-            if (
-                len(self.obs_buf[i, idx * self.num_obs : (idx + 1) * self.num_obs])
-                != 51
-            ):
+            obs.append(self.obs_buf[i, idx * self.num_obs])
+            if len(self.obs_buf[i, idx * self.num_obs]) != 51:
                 print("AAAAA")
-                print(
-                    len(self.obs_buf[i, idx * self.num_obs : (idx + 1) * self.num_obs])
-                )
+                print(len(self.obs_buf[i, idx * self.num_obs]))
         print("===")
         return torch.stack(obs, dim=0)
