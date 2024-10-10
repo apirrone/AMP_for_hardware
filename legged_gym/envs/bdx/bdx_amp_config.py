e4a8869511bcda0e93a051ea4d56c30d8df79d2d
apirrone <awanegenebayo@gmail.com>
2024-10-10 23:25:54 +0200

    rand

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -192,9 +192,9 @@ class BDXAMPCfg(LeggedRobotCfg):
 
     class domain_rand:
         randomize_friction = True
-        friction_range = [0.9, 1.1]
+        friction_range = [0.8, 1.2]
         randomize_base_mass = True
-        added_mass_range = [-0.01, 0.01]
+        added_mass_range = [-0.05, 0.05]
         push_robots = False
         push_interval_s = 3
         max_push_vel_xy = 0.5  # 0.3
@@ -202,9 +202,9 @@ class BDXAMPCfg(LeggedRobotCfg):
         stiffness_multiplier_range = [0.99, 1.01]
         damping_multiplier_range = [0.99, 1.01]
         randomize_torques = True
-        torque_multiplier_range = [0.95, 1.05]
+        torque_multiplier_range = [0.9, 1.1]
         randomize_com = True
-        com_range = [-0.01, 0.01]
+        com_range = [-0.05, 0.05]
         observation_lag = True
         observation_lag_range = [0, 1]  # ms
 
@@ -213,11 +213,11 @@ class BDXAMPCfg(LeggedRobotCfg):
         noise_level = 1.0  # scales other values
 
         class noise_scales:
-            dof_pos = 0.01
-            dof_vel = 0.01  # finish with very large dof_vel ? 1.5
-            lin_vel = 0.01
-            ang_vel = 0.01
-            gravity = 0.01
+            dof_pos = 0.1
+            dof_vel = 0.1  # finish with very large dof_vel ? 1.5
+            lin_vel = 0.1
+            ang_vel = 0.1
+            gravity = 0.1
             height_measurements = 0.1
 
     class rewards(LeggedRobotCfg.rewards):
