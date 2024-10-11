98ff32d183a49ec7a7b8cfad935fc403f1954d86
apirrone <awanegenebayo@gmail.com>
2024-10-11 10:11:59 +0200

    starting again with disc grad pen 10

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -192,9 +192,9 @@ class BDXAMPCfg(LeggedRobotCfg):
 
     class domain_rand:
         randomize_friction = True
-        friction_range = [0.7, 1.3]
+        friction_range = [0.9, 1.1]
         randomize_base_mass = True
-        added_mass_range = [-0.1, 0.1]
+        added_mass_range = [-0.01, 0.01]
         push_robots = False
         push_interval_s = 3
         max_push_vel_xy = 0.5  # 0.3
@@ -202,9 +202,9 @@ class BDXAMPCfg(LeggedRobotCfg):
         stiffness_multiplier_range = [0.99, 1.01]
         damping_multiplier_range = [0.99, 1.01]
         randomize_torques = True
-        torque_multiplier_range = [0.8, 1.1]
+        torque_multiplier_range = [0.95, 1.05]
         randomize_com = True
-        com_range = [-0.1, 0.1]
+        com_range = [-0.01, 0.01]
         observation_lag = True
         observation_lag_range = [0, 1]  # ms
 
@@ -213,11 +213,11 @@ class BDXAMPCfg(LeggedRobotCfg):
         noise_level = 1.0  # scales other values
 
         class noise_scales:
-            dof_pos = 0.2
-            dof_vel = 0.2  # finish with very large dof_vel ? 1.5
-            lin_vel = 0.2
-            ang_vel = 0.2
-            gravity = 0.2
+            dof_pos = 0.01
+            dof_vel = 0.01  # finish with very large dof_vel ? 1.5
+            lin_vel = 0.01
+            ang_vel = 0.01
+            gravity = 0.01
             height_measurements = 0.1
 
     class rewards(LeggedRobotCfg.rewards):
@@ -254,9 +254,9 @@ class BDXAMPCfg(LeggedRobotCfg):
         heading_command = False  # if true: compute ang vel command from heading error
 
         class ranges:
-            lin_vel_x = [0.0, 0.14]  # min max [m/s] # 0.14 ok
-            lin_vel_y = [-0.1, 0.1]  # min max [m/s] # O.1 ok
-            ang_vel_yaw = [-0.3, 0.3]  # min max [rad/s] # 0.3 ok
+            lin_vel_x = [0.14, 0.14]  # min max [m/s] # 0.14 ok
+            lin_vel_y = [0.0, 0.0]  # min max [m/s] # O.1 ok
+            ang_vel_yaw = [0.0, 0.0]  # min max [rad/s] # 0.3 ok
             heading = [0, 0]
 
     class viewer(LeggedRobotCfg.viewer):
@@ -291,6 +291,6 @@ class BDXAMPCfgPPO(LeggedRobotCfgPPO):
         amp_task_reward_lerp = 0.3  # 0.3
         amp_discr_hidden_dims = [1024, 512]
 
-        disc_grad_penalty = 0.01  # original 10 # TUNE ?
+        disc_grad_penalty = 10  # original 10 # TUNE ?
 
         min_normalized_std = [0.02] * 15
