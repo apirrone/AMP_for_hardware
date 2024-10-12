65ff2f34c832954704459c68472d856dfacd1db8
apirrone <awanegenebayo@gmail.com>
2024-10-12 16:09:07 +0200

    noise

--- legged_gym/envs/bdx/bdx_amp_config.py
+++ legged_gym/envs/bdx/bdx_amp_config.py
@@ -214,11 +214,11 @@ class BDXAMPCfg(LeggedRobotCfg):
         noise_level = 1.0  # scales other values
 
         class noise_scales:
-            dof_pos = 0.2
-            dof_vel = 0.2  # finish with very large dof_vel ? 1.5
-            lin_vel = 0.2
-            ang_vel = 0.2
-            gravity = 0.2
+            dof_pos = 0.1
+            dof_vel = 0.1  # finish with very large dof_vel ? 1.5
+            lin_vel = 0.1
+            ang_vel = 0.1
+            gravity = 0.1
             height_measurements = 0.1
 
     class rewards(LeggedRobotCfg.rewards):
